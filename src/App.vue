b18ea75157525446cfc6f903e26fc929fbab94bb
JrzenonDev <jrobertoonb@gmail.com>
2021-07-13 01:06:48 -0300

    chore: create component Home

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <Header />
+    <Home />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Home from './pages/Home.vue';
 
 import './assets/css/globals.css';
 
@@ -13,6 +15,7 @@ export default {
   name: 'App',
   components: {
     Header,
+    Home,
   }
 }
 </script>
