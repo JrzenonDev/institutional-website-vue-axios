93a08dc226b436076fc8755b1b2f4af4517e0a2b
JrzenonDev <jrobertoonb@gmail.com>
2021-07-12 21:58:12 -0300

    chore: clearing vue base structure and placing initial styles

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import './css/globals.css';
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
