c29999f85d1bf9c0a748e62508bd83e7a2f10029
JrzenonDev <jrobertoonb@gmail.com>
2021-07-13 01:46:35 -0300

    chore: create component footer, using axios to query the api and fetch the data to use in the component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div id="app">
     <Header />
     <Home />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
 import Home from './pages/Home.vue';
+import Footer from './components/Footer.vue';
 
 import './assets/css/globals.css';
 
@@ -16,6 +18,7 @@ export default {
   components: {
     Header,
     Home,
+    Footer,
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,62 @@
+<template>
+  <footer>
+
+    <a
+      v-for="(socialLink, index) in socialLinks"
+      :key="index"
+      :href="socialLink.link"
+      class="socialLinks"
+    >
+      <img
+        :src="socialLink.icon"
+        :title="socialLink.label"
+        :alt="socialLink.label"
+      >
+    </a>
+
+  </footer>
+</template>
+
+<script>
+import api from '@/services/api.js';
+
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      socialLinks: [],
+    }
+  },
+  mounted() {
+    api.get('/social-links.json').then(response => {
+      this.socialLinks = response.data;
+    });
+  }
+}
+</script>
+
+<style>
+  footer {
+    width: 100%;
+    height: 60px;
+    background-color: var(--color-background-nav);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .socialLinks img {
+    border: 2px solid var(--color-text-light);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    width: 40px !important;
+    height: 40px;
+    margin: 0 10px;
+  }
+
+  .socialLinks img {
+    width: 20px;
+  }
+</style>
