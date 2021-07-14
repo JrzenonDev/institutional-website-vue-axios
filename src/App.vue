ce35f47b7a5bee6fffe77b65511abe639af295b7
JrzenonDev <jrobertoonb@gmail.com>
2021-07-13 21:04:20 -0300

    create routes in application

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="app">
     <Header />
-    <Home />
+    <router-view path="$router.key" />
     <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
-import Home from './pages/Home.vue';
 import Footer from './components/Footer.vue';
 
 import './assets/css/globals.css';
@@ -17,7 +16,6 @@ export default {
   name: 'App',
   components: {
     Header,
-    Home,
     Footer,
   }
 }
